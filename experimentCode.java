43c742f3993a739ac28febf036f2f7763877609e
Omer Drori <73065220+Omerd05@users.noreply.github.com>
2024-03-13 19:55:13 +0200

    Update experimentCode.java

--- experimentCode.java
+++ experimentCode.java
@@ -12,7 +12,7 @@ public class experimental {
             double avgTrees = 0;
             double avgRanksDeleted = 0;
 
-            int precise = 30;
+            int precise = 1;
             int temp_precise = precise;
             while(temp_precise-- > 0){
                 BinomialHeap BH = new BinomialHeap();
@@ -31,9 +31,11 @@ public class experimental {
             avgTrees /= precise;
             avgRanksDeleted /= precise;
             System.out.println("Time for the " + i + "'th experiment : " + String.valueOf(avgTime));
+            System.out.println("Size : " + String.valueOf(Math.pow(3,i+5)-1));
             System.out.println("Total number of links : " + String.valueOf(avgLinks));
             System.out.println("Number of trees : " + String.valueOf(avgTrees));
             System.out.println("Sum of deleteds' rank : " + String.valueOf(avgRanksDeleted));
+            System.out.println();
         }
     }
 
@@ -45,8 +47,9 @@ public class experimental {
             double avgLinks = 0;
             double avgTrees = 0;
             double avgRanksDeleted = 0;
+            double avgSize = 0;
 
-            int precise = 30;
+            int precise = 1;
             int temp_precise = precise;
             while(temp_precise-- > 0){
                 BinomialHeap BH = new BinomialHeap();
@@ -69,6 +72,7 @@ public class experimental {
                 avgTime+= endTime-startTime;
                 avgLinks += BH.linksCount;
                 avgTrees += BH.numTrees();
+                avgSize += BH.size;
                 avgRanksDeleted += BH.ranksDeleted;
             }
 
@@ -77,14 +81,119 @@ public class experimental {
             avgTrees /= precise;
             avgRanksDeleted /= precise;
             System.out.println("Time for the " + i + "'th experiment : " + String.valueOf(avgTime));
+            System.out.println("Start size : " + String.valueOf(Math.pow(3,i+5)-1));
+            System.out.println("End size : " + String.valueOf(avgSize));
             System.out.println("Total number of links : " + String.valueOf(avgLinks));
             System.out.println("Number of trees : " + String.valueOf(avgTrees));
             System.out.println("Sum of deleteds' rank : " + String.valueOf(avgRanksDeleted));
+            System.out.println();
+        }
+    }
+
+    public static void thirdExperiment(){
+        System.out.println("Third experiment");
+        for(int i = 1; i <= 6; i++){
+            int N = (int)(Math.pow(3,i+5)-1);
+            double avgTime = 0;
+            double avgLinks = 0;
+            double avgTrees = 0;
+            double avgRanksDeleted = 0;
+            double avgSize = 0;
+
+            int precise = 1;
+            int temp_precise = precise;
+            while(temp_precise-- > 0){
+                BinomialHeap BH = new BinomialHeap();
+                ArrayList<Integer> A = new ArrayList<>();
+                for(int j = 0; j<N; j++){
+                    A.add(N-j);
+                }
+                //Collections.shuffle(A);
+
+                long startTime = System.currentTimeMillis();
+
+                for(int j = 0; j<N; j++){
+                    BH.insert(A.get(j),String.valueOf(A.get(j)));
+                }
+                while (BH.size >= 1 << 5){
+                    BH.deleteMin();
+                }
+
+                long endTime = System.currentTimeMillis();//-starting_point;
+                avgTime+= endTime-startTime;
+                avgLinks += BH.linksCount;
+                avgTrees += BH.numTrees();
+                avgSize += BH.size;
+                avgRanksDeleted += BH.ranksDeleted;
+            }
+
+            avgTime /= precise;
+            avgLinks /= precise;
+            avgTrees /= precise;
+            avgRanksDeleted /= precise;
+            System.out.println("Time for the " + i + "'th experiment : " + String.valueOf(avgTime));
+            System.out.println("Start size : " + String.valueOf(Math.pow(3,i)-1));
+            System.out.println("End size : " + String.valueOf(avgSize));
+            System.out.println("Total number of links : " + String.valueOf(avgLinks));
+            System.out.println("Number of trees : " + String.valueOf(avgTrees));
+            System.out.println("Sum of deleteds' rank : " + String.valueOf(avgRanksDeleted));
+            System.out.println();
+        }
+    }
+
+    public static void intuiation(){
+        System.out.println("Intuiation experiment");
+        for(int i = 1; i <= 6; i++){
+            int N = (int)(Math.pow(3,i)-1);
+            double avgTime = 0;
+            double avgLinks = 0;
+            double avgTrees = 0;
+            double avgRanksDeleted = 0;
+            double avgSize = 0;
+
+            int precise = 1;
+            int temp_precise = precise;
+            while(temp_precise-- > 0){
+                BinomialHeap BH = new BinomialHeap();
+                ArrayList<Integer> A = new ArrayList<>();
+                for(int j = 1; j<=N; j++){
+                    A.add(j);
+                }
+                Collections.shuffle(A);
+
+                long startTime = System.currentTimeMillis();
+
+                for(int j = 0; j<N; j++){
+                    BH.insert(A.get(j),String.valueOf(A.get(j)));
+                }
+                for(int t = 0; t< 1; t++){
+                    BH.deleteMin();
+                }
+
+                long endTime = System.currentTimeMillis();//-starting_point;
+                avgTime+= endTime-startTime;
+                avgLinks += BH.linksCount;
+                avgTrees += BH.numTrees();
+                avgRanksDeleted += BH.ranksDeleted;
+                avgSize = BH.size;
+            }
+
+            avgTime /= precise;
+            avgLinks /= precise;
+            avgTrees /= precise;
+            avgRanksDeleted /= precise;
+            System.out.println("Time for the " + i + "'th experiment : " + String.valueOf(avgTime));
+            System.out.println("Start size : " + String.valueOf(Math.pow(3,i)-1));
+            System.out.println("End size : " + String.valueOf(avgSize));
+            System.out.println("Total number of links : " + String.valueOf(avgLinks));
+            System.out.println("Number of trees : " + String.valueOf(avgTrees));
+            System.out.println("Sum of deleteds' rank : " + String.valueOf(avgRanksDeleted));
+            System.out.println();
         }
     }
 
     public static void main(String[] args){
-        secondExperiment();
+        //thirdExperiment();
         /*int A[] = {5, 4, 2, 3, 1 };
         BinomialHeap BH = new BinomialHeap();
         int N = A.length;
